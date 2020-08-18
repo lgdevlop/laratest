33ac759b8e38b133fba4da8a067b5eb022d60946
Leonardo Gomes <lgdevlop@gmail.com>
2020-08-18 00:13:19 -0300

    feat: :wrench: atualizando versao docker compose utilizando script para verificar se o db esta pronto

--- config.sh
+++ config.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
 
-echo $DB_HOST_NAME
+export MAGENTABG='\033[45m'
+export WHITE='\033[97m'
+export RESET='\033[0m'
 
+echo ""
+echo -e "${MAGENTABG}${WHITE} Tentando conectar em: http://${DB_HOST_NAME}:3306 ${RESET}"
 health=$(curl -v "http://$DB_HOST_NAME:3306" 2>&1 | grep -o "Connected to")
+retries=0
+max_retries=4
 
 while [ -z "$health" ]; do
-  sleep 3s
-  echo $DB_HOST_NAME
+  if [[ "$retries" = "$max_retries" ]]; then
+    echo ""
+    exit
+  fi
+  sleep 4s
+  echo -e "${MAGENTABG}${WHITE} Tentando conectar em: http://${DB_HOST_NAME}:3306 ${RESET}"
   health=$(curl -v "http://$DB_HOST_NAME:3306" 2>&1 | grep -o "Connected to")
+  ((retries=$retries+1))
 done
 
 composer install
